c385a103b45e56b33f137303a89c0f43ea9a7b42
= <felipesampaio93@hotmail.com>
2015-10-04 21:37:57 -0300

    Creating domain class Result

--- gradPlanner/src/main/java/br/com/compositeam/gradplanner/model/Result.java
+++ gradPlanner/src/main/java/br/com/compositeam/gradplanner/model/Result.java
@@ -1,5 +1,61 @@
 package br.com.compositeam.gradplanner.model;
 
 public class Result {
+	
+	private Student student;
+	//private ClassName classname; Commented because class not implemented yet.
+	private String description;
+	private String mention;
+	private int period;
+	
+	//Empty constructor
+	public Result()
+	{
+		
+	}
+	
+	//Full constructor
+	public Result(Student student, String description, String mention, int period)
+	{
+		this.student = student;
+		this.description = description;
+		this.mention = mention;
+		this.period = period;
+	}
 
+	/*Getters and setters*/
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getMention() {
+		return mention;
+	}
+
+	public void setMention(String mention) {
+		this.mention = mention;
+	}
+
+	public int getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(int period) {
+		this.period = period;
+	}
+	
+
+	
 }
